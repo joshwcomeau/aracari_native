0c6954af0fa86370e5fbe3b04631049eaa0a4ecc
Joshua Comeau <joshwcomeau@gmail.com>
2016-05-19 07:53:19 -0400

    Set up very basic navigator routing

--- index.ios.js
+++ index.ios.js
@@ -7,17 +7,24 @@ import {
   View
 } from 'react-native';
 
-import BudgetView from './src/components/ios/BudgetView'
+import BudgetView from './src/components/ios/BudgetView';
+import NewBudgetItem from './src/components/ios/NewBudgetItem';
+
 
 class aracarinative extends Component {
+  renderScene(route, navigator) {
+    switch (route.name) {
+      case 'Budget': return <BudgetView navigator={navigator} />;
+      case 'NewBudgetItem': return <NewBudgetItem />;
+    }
+  }
+
   render() {
     return (
       <Navigator
         style={{ flex: 1 }}
-        initialRoute={{name: 'Aracari', index: 0}}
-        renderScene={(route, navigator) =>
-          <BudgetView />
-        }
+        initialRoute={{name: 'Budget', index: 0}}
+        renderScene={this.renderScene}
       />
     );
   }
--- src/components/ios/BudgetView/index.js
+++ src/components/ios/BudgetView/index.js
@@ -2,15 +2,28 @@ import React, { Component, PropTypes } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  TouchableHighlight,
   Text,
   View
 } from 'react-native';
 
 class BudgetView extends Component {
+  constructor(...args) {
+    super(...args);
+    this.addNewBudgetItem = this.addNewBudgetItem.bind(this);
+  }
+
+  addNewBudgetItem() {
+    this.props.navigator.push({ name: 'NewBudgetItem' })
+  }
+
   render() {
     return (
       <View style={styles.budgetView}>
         <Text>Budget View</Text>
+        <TouchableHighlight onPress={this.addNewBudgetItem}>
+          <Text>Add New Budget Item</Text>
+        </TouchableHighlight>
       </View>
     )
   }
