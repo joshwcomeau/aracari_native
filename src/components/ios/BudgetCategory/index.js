fbfead8b3a567b0f4e995f6c653db1dc103b8030
Joshua Comeau <joshwcomeau@gmail.com>
2016-05-19 21:55:39 -0400

    Added budget bars

--- src/components/ios/BudgetCategory/index.js
+++ src/components/ios/BudgetCategory/index.js
@@ -6,19 +6,42 @@ import {
   View
 } from 'react-native';
 
+import { getBudgetColour, formatHSLColourForCSS } from '../../../utils/colour.utils';
+
+// const BudgetCategory = ({
+//
+// }) => {
+//   const budgetProgressStyle = {
+//     width: budgetPercentage,
+//     backgroundColor: formatHSLColourForCSS(budgetColour),
+//   };
+//
+// }
+
 class BudgetCategory extends Component {
   render() {
-    console.log("Rendering", this.props)
+    const {
+      name, slug, budget, amountSpent, monthProgress, actions,
+    } = this.props;
+
+    const budgetRatio = amountSpent / budget;
+    const budgetPercentage = budgetRatio * 100;
+    const budgetColour = formatHSLColourForCSS(getBudgetColour(budgetRatio, monthProgress));
+
     return (
-      <View style={styles.budgetView}>
-        <Text>{this.props.name}</Text>
+      <View style={styles.budgetCategory}>
+        <View style={styles.budgetBarContainer}>
+          <View style={[styles.budgetBar, { flex: budgetPercentage, backgroundColor: budgetColour }]} />
+          <View style={[styles.budgetSpacer, { flex: 100 - budgetPercentage }]} />
+        </View>
+        <Text style={styles.budgetLabel}>{name}</Text>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  budgetView: {
+  budgetCategory: {
     alignSelf: 'stretch',
     margin: 4,
     height: 40,
@@ -28,7 +51,27 @@ const styles = StyleSheet.create({
     shadowColor: '#000000',
     shadowOffset: { width: 0.2, height: 0.2 },
     shadowOpacity: 0.4,
-    shadowRadius: 0.5
+    shadowRadius: 0.5,
+  },
+  budgetBar: {
+    height: 40,
+  },
+  budgetSpacer: {
+    height: 40,
+  },
+  budgetBarContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  budgetLabel: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    left: 0,
+    right: 0,
   },
 });
 
